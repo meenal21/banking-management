b796036a60c363e52cf56e5617cebe1d6cf01ffe
meenal21 <mpbjain@gmail.com>
2024-10-19 04:26:48 +0530

    Adding updated emmployee customer files

--- customer.h
+++ customer.h
@@ -6,7 +6,7 @@
 #include "./placeholder.h"
 #include "./getcounter.h"
 
-int createcust(){
+int create_customer(){
     int fd;
     int id;
     struct Customer cust;
@@ -14,8 +14,6 @@ int createcust(){
     char message[BUFFER_SIZE];
     char const *filename = "customer.txt";
     
-    
-
     //ssize_t bytes_read;
     ssize_t bytes_writ;
     fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0777);
@@ -33,52 +31,52 @@ int createcust(){
     printf("Enter customer details: \n");
     
     printf("Name: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.name,message);
     
     printf("DOB: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.dob,message);
     
     printf("Ph. Number: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.number,message);
 
     printf("Address: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.address,message);
 
     printf("Document Type: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.iddoc,message);
 
     printf("ID Number: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.idnumber,message);
 
     printf("Balance: ");
-    do{
+    //do{
 		fgets(message, BUFFER_SIZE, stdin);
-		message[strcspn(message, "\n")] = '\0'; // removing new line character
-	}while(strlen(message)==0);
+	//	message[strcspn(message, "\n")] = '\0'; // removing new line character
+	//}while(strlen(message)==0);
     strcpy(cust.idnumber,message);
     cust.balance = atoi(message);
 
@@ -97,8 +95,8 @@ int createcust(){
     cust.active = true;
     cust.loggedin = false;
     
-    lseek(fd, 0 , SEEK_END);
-    if((bytes_writ = write(fd,&cust,sizeof(struct Customer))) < bytes_writ){
+    bytes_writ = write(fd,&cust,sizeof(struct Customer));
+    if(bytes_writ < 0){
         perror("Creating customer failed");
         close(fd);
         return -1;
@@ -109,4 +107,29 @@ int createcust(){
     return id;
 }
 
+int read_customer(struct Customer * cust, int cid) {
+    int fd;
+    struct Customer temp_cust;
+    ssize_t bytes_read;
+
+    if ((fd = open("customer.txt", O_RDONLY)) < 0) {
+        perror("Error opening the file");
+        return -1; // Return an empty struct
+    }
+
+    while ((bytes_read = read(fd, &temp_cust, sizeof(struct Customer))) > 0){
+        if (temp_cust.cid == cid) {
+            close(fd);
+            *cust = temp_cust;
+            return 1; // Return the found customer
+        }
+    }
+
+    if (bytes_read < 0) {
+        perror("Could not read");
+    }
+
+    close(fd);
+    return -1; // Return an empty struct if not found
+}
 #endif 
